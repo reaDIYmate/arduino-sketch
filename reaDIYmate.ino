39d8f2800d73b6d64df7e6d7b69a3285accb6c14
omevel <olivier@readiymate.com>
2012-12-17 14:28:29 +0100

    Add reaDIYmate.ino

---
+++ reaDIYmate.ino
@@ -0,0 +1,50 @@
+#include <Configuration.h>
+#include <eepromAddresses.h>
+#include <Facebook.h>
+#include <Gmail.h>
+#include <Inbox.h>
+#include <Resources.h>
+#include <Settings.h>
+#include <Twitter.h>
+#include <SoundCloud.h>
+#include <SdFat.h>
+#include <Servo.h>
+
+
+#include <PusherTrajectory.h>
+#include <SdTrajectory.h>
+#include <ServoControl.h>
+#include <Trajectory.h>
+
+#include <Api.h>
+#include <Audio.h>
+#include <BufferedStream.h>
+#include <digitalWriteFast.h>
+#include <Download.h>
+#include <ExtendedStream.h>
+#include <HttpClient.h>
+#include <JsonStream.h>
+#include <PusherClient.h>
+#include <SerialStream.h>
+#include <Sha256.h>
+#include <StatusLed.h>
+#include <Vs1011.h>
+#include <Wifly.h>
+
+#include <Button.h>
+#include <Dispatcher.h>
+#include <Fsm.h>
+#include <Motion.h>
+#include <Personality.h>
+#include <Player.h>
+#include <Signals.h>
+
+Dispatcher dispatcher;
+
+void setup () {
+    dispatcher.setup();
+}
+
+void loop() {
+    dispatcher.loop();
+}
